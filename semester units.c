5f747bfdc4fb2ac243f2e439c2aa7d8578640666
fwleorn <143502495+fwleorn@users.noreply.github.com>
2025-09-24 23:23:43 +0300

    Add files via upload

---
+++ semester units.c
@@ -0,0 +1,23 @@
+/*
+name: lyberntee mutinda
+reg: PA106/G/26642/25
+DESCRIPTION: program that displays all my units for the semester
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("1.SPC2101:programming\n");
+    printf("2.SPM2100:discrete math\n");
+    printf("3.UCU2105:fundamentals of ict\n");
+    printf("4.SPI2102:computer systems and organization\n");
+    printf("5.UCU2101:communication skills\n");
+    printf("6.SPM2104:mathematics for science\n");
+    printf("7.SSE2101:system analysis and design\n");
+    return 0;
+}
+
+//end of the program
